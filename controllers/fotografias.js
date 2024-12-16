685dbad8387ac26f0f6ac9a63ae7a8e76633ffcc
cesar_ramirez <cesar_ramirez@hacienda.gov.py>
2024-12-16 16:37:24 -0300

    Se agrega subir foto y obtener foto

--- controllers/fotografias.js
+++ controllers/fotografias.js
@@ -1,4 +1,7 @@
 const Fotografias = require('../models/fotografia')
+const thumb= require('node-thumbnail').thumb;
+const path = require('path')
+const fs= require('fs')
 
 const create = async (req, res) => {
     try {
@@ -51,4 +54,105 @@ const update = async (req, res) => {
     }
 }
 
-module.exports = {create, update}
+const uploadFotografia = async (req, res) => {
+    try {
+        const id= req.params.id
+
+        if(req.files) {
+            const file_path=req.files.foto.path
+            console.log(req.files)
+            const file_split= file_path.split('\\')
+            const file_name = file_split[2]
+            const ext_split= file_name.split('\.')
+            const file_ext= ext_split[1]
+            if(file_ext=='jpg'){
+                let fotografia={}
+                const objetoBD = await Fotografias.findById(id)
+                if(!objetoBD) {
+                    fs.unlink(file_path, (err)=> {
+                        if(err) {
+                            res.status(500).json({
+                                message: 'Error al tratar el archivo'
+                            })
+                        }
+                    })
+                    return res.status(404).json({
+                        message: 'No existe el registro con el id buscado'
+                    })
+                }
+                fotografia.imagen=file_name
+          
+                const fotoActualizada = await Fotografias.findByIdAndUpdate(id, fotografia, {new: true})
+
+                const new_path = './uploads/fotografias/' + file_name
+                const thumb_path = './uploads/fotografias/thumbs'
+
+                thumb({
+                    source: path.resolve(new_path),
+                    destination: path.resolve(thumb_path),
+                    width: 200,
+                    suffix: '',
+                    ignore: false
+                }).then(()=>{
+                    res.status(200).json({
+                        message: 'Archivo subido',
+                        fotoActualizada
+                    })
+                }).catch((err)=>{
+                    res.status(500).json({
+                        message: 'Error al crear el thumbnail'
+                    })
+                })
+            } else {
+                fs.unlink(file_path, (err)=> {
+                    if(err) {
+                        res.status(500).json({
+                            message: 'Error al tratar el archivo'
+                        })
+                    }
+                })
+                return res.status(400).json({
+                    message: 'Solo se permite archivos JPG'
+                })
+            }
+        } else {
+            return res.status(400).json({
+                message: 'No hay ningún archivo para subir'
+            })
+        }
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({
+            message: 'Error inesperado'
+        })
+    }
+}
+
+const getFotografia = (req, res) => {
+    try {
+        const fotografia = req.params.fotografia
+        const thumb= req.params.thumb
+        let path_foto=null
+        if(!thumb)
+            path_foto='./uploads/fotografias/' + fotografia
+        else
+            path_foto='./uploads/fotografias/thumbs/' + fotografia
+
+        fs.exists(path_foto, (exists)=>{
+            if(exists){
+                res.sendFile(path.resolve(path_foto))
+            }else{
+                res.status(500).json({
+                    message: 'No se encuentra la fotografía'
+                })
+            }
+        })
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({
+            message: 'Error inesperado'
+        })
+    }
+}
+
+module.exports = {create, update, uploadFotografia, getFotografia}
--- routes/fotografia.js
+++ routes/fotografia.js
@@ -2,10 +2,14 @@ const express = require('express');
 const router = express.Router();
 const fotografias = require('../controllers/fotografias');
 const md_auth = require('../middlewares/verifyJWT')
+const cm = require('connect-multiparty')
 
+const md_upload = cm({uploadDir: './uploads/fotografias'})
 
 //router.get('/listar', md_auth.verifyAuth, usuario.list);
 router.post('/guardar', fotografias.create);
 router.post('/actualizar/:id', fotografias.update);
+router.post('/subir-foto/:id', md_upload, fotografias.uploadFotografia)
+router.get('/get-foto/:fotografia/:thumb', md_upload, fotografias.getFotografia)
 
 module.exports = router;
